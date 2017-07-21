5ad9ac8a71e864b9734bfdd581e7628270bdd57a
zlodiak <prozaik81-2@yandex.ru>
2017-07-21 03:21:04 +0300

    guest form ok

--- guestbook/views.py
+++ guestbook/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.http import JsonResponse
+import json
 
 from .models import Message
 
 
 def message_create(request):
-		m = Message(name='qwe', text='fgfjgj', published_date=timezone.now(), created_date=timezone.now());
-		m.save();
+		if (len(request.POST['message']) > 200) or (len(request.POST['name']) > 60):
+			status = '0'
+			error_message = 'Хакир штоль?'
+		else:
+			status = '1'
+			error_message = ''
+			m = Message(name=request.POST['name'], text=request.POST['message'], published_date=timezone.now(), created_date=timezone.now(),)
+			m.save();
+
+		data = json.dumps({ 'status': status , 'error_message': error_message })
+
+		return JsonResponse(data, safe=False)     		
